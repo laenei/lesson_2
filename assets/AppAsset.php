56a488b6246d99601a0a0a3c27f2912c27904a5b
maxim <laenei@ya.ru>
2018-02-04 19:05:51 +0800

    test

--- assets/AppAsset.php
+++ assets/AppAsset.php
@@ -21,7 +21,7 @@ class AppAsset extends AssetBundle
     public $baseUrl = '@web';
     public $css = [
         'css/site.css',
-        'css/main-menu.css',
+        'css/menu.css'
     ];
     public $js = [
     ];
